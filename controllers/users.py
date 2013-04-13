12a7720457e3d385f80eb18f4c59313ac6ba9583
Rui Carmo <rui.carmo@gmail.com>
2013-04-13 22:21:54 +0100

    Return feeds for user

--- controllers/users.py
+++ controllers/users.py
@@ -3,7 +3,7 @@ import os, sys, logging
 
 log = logging.getLogger()
 
-from models import User, Group, Subscription, db
+from models import User, Group, Subscription, Feed, Item, Read, db
 from config import settings
 from decorators import cached_method
 from collections import defaultdict
@@ -43,6 +43,22 @@ class UserController:
   
 
     @cached_method
+    def get_feeds_for_user(self, user):
+        q = Feed.select(Feed).join(Subscription).join(User).where(User.id == user.id).distinct().naive()
+        result = [{
+            'id'                  : f.id,
+            'favicon_id'          : f.favicon,
+            'title'               : f.title,
+            'url'                 : f.url,
+            'site_url'            : f.site_url,
+            'is_spark'            : 0, # TODO: implement this field in the model
+            'last_updated_on_time': f.last_checked
+        } for f in q]
+        db.close()
+        return result
+
+
+    @cached_method
     def get_groups_for_user(self, user):
         q = Group.select(Group).join(Subscription).join(User).where(User.id == user.id).distinct().naive()
         result = [{'id':s.id,'title':s.title} for s in q]
@@ -56,12 +72,20 @@ class UserController:
         groups = defaultdict(lambda: [])
         for s in q:
             groups[str(s.group.id)].append('%d' % s.feed.id)
-        log.debug(groups)
         result = []
         for g in groups.keys():
             result.append({'group':g, 'feed_ids':','.join(groups[g])})
         return result
              
+
+    @cached_method
+    def get_unread_items_for_user(self, user):
+        q = Item.select(Item.id).join(Feed).join(Subscription).join(User).where(
+            (User.id == user.id), 
+            ~(Item.id << Read.select(Read.item).where(User.id == user.id))).distinct().naive()
+        return ','.join([str(r.id) for r in q])
+         
+
     def add_feed_to_group(self, user, feed, group):
         s = Subscription.create(user = user, feed = feed, group = group)
         db.close()
